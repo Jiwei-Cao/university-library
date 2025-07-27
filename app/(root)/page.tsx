5f4debe73e32bbf84359f183d8180102cb10fb2d
Jiwei-Cao <jiweicao2006@gmail.com>
2025-07-27 14:10:39 +0100

    added props

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,13 @@
-import BookList from "@/components/BookList"
 import BookOverview from "@/components/BookOverview"
+import BookList from "@/components/BookList"
+import { sampleBooks } from "@/constants";
 
 const Home = () => {
   return (
     <>
-      <BookOverview />
+      <BookOverview {...sampleBooks[0]}/>
 
-      <BookList />
+      <BookList title="Latest Books" books={sampleBooks} containerClassName="mt-28"/>
     </>
   );
 }
