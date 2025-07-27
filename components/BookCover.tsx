4a5bf60d9312dd555a50684f704a53eeb0e28e93
Jiwei-Cao <jiweicao2006@gmail.com>
2025-07-27 18:43:22 +0100

    updated config links

--- components/BookCover.tsx
+++ components/BookCover.tsx
@@ -1,13 +1,34 @@
 import React from "react";
+import Image from "next/image"
+import { cn } from "@/lib/utils"
 
-const BookList = () => {
+type BookCoverVariant = "extraSmall" | "small" | "medium" | "regular" | "wide";
+
+const variantStyles: Record<BookCoverVariant, string> = {
+    extraSmall: "book-cover_extra_small",
+    small: "book-cover_small",
+    medium: "book-cover_medium",
+    regular: "book-cover_regular",
+    wide: "book-cover_wide"
+}
+
+interface Props {
+    className?: string;
+    variant: BookCoverVariant;
+    coverColor: string;
+    coverImage: string;
+}
+
+const BookCover = ({ className, variant="regular", coverColor="#012B48", coverImage="https://placehold.co/400x600.png" }: Props) => {
     return (
-        <section>
-            <h2 className="font-bebas-neue text-4xl text-light-100">
-                Popular Books
-            </h2>
-        </section>
+        <div className={cn('relative transition-all duration-300', variantStyles[variant], className)}>
+            BOOK SIDE SVG 
+
+            <div className="absolute z-10" style={{ left: '12%', width: '87.5%', height: '88%' }}>
+                <Image src={coverImage} alt="Book cover" fill className="rounded-sm object-fill"/>
+            </div>
+        </div>
     );
 };
 
-export default BookList;
+export default BookCover;
--- components/BookList.tsx
+++ components/BookList.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const BookList = () => {
     return (
-        <section>
-            <h2 className="font-bebas-neue text-4xl text-light-100">Popular Books</h2>
-        </section>
+        <div>
+            <p>book list</p>
+        </div>
     )
 };
 
